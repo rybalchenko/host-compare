1a6649fa053237f2abd0b22e1b6b4a1ff04ccf21
rybalchenko <pavel3220@mail.ru>
2016-02-15 11:26:45 +0200

    fixed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 $(document).foundation();
+
+;(function($){
+
+	$(function(){
+		$('.ba-slider').slick({
+			infinite: false
+		});
+	});
+
+})(jQuery);
+
+;(function($){
+
+	$(function(){
+		$('.ba-features-slider').slick({
+			infinite: false,
+			dots: true,
+			arrow: false,
+			prevArrow: false,
+			nextArrow:false
+		});
+	});
+
+})(jQuery);
